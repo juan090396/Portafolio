daaa69fe2d572d4e35263819badc06616ed4859d
juan090396 <juanfgb093@gamil.com>
2023-12-26 17:31:24 -0300

    inicio de proyecto de Portafolio

---
+++ Portafolio/src/App.jsx
@@ -0,0 +1,46 @@
+
+
+function App() {
+
+
+  return (
+    <div className="container" >
+      <nav className= " "  > aqui va el modo oscuro y la preferencia de idioma </nav>
+
+
+      <div>
+        <h1 className="text-3xl font-bold underline">Juan Figueroa</h1>
+        
+          <h2>Web Developer</h2>
+          <div>
+          <p>aqui va una imagen de una tecnologia</p>
+          <p>aqui va una imagen de una tecnologia</p>
+          <p>aqui va una imagen de una tecnologia</p>
+          </div>
+
+      <div>
+        <button>boton para descargar cv</button>
+        <button>boton para ver linkedin</button>
+      </div>
+    <hr/>
+      </div>
+
+      <div>
+        <h2>Experiencia</h2>
+        <p>aqui va la experiencia</p>
+      </div>
+
+      <div>
+      <h2>Proyectos</h2>
+        <p>aqui van los proyectos</p>
+      </div>
+
+      <footer>
+      <p>Contacto</p>
+      <p>aqui van las redes</p>
+      </footer>
+  </div>
+  )
+}
+
+export default App
